4bc588fe7310ca36e7be64cd2fe40c13f19e92c3
Bymbacz <mateusz.bywalec2002@gmail.com>
2023-03-27 12:48:43 +0200

    GUI

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pygame, sys
 
-print()
-print("hej")
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280,720)) # rozdzielczosc do zmiany, pozniej wypelniana z ustawien (settings.py)
+        pygame.display.set_caption('Projekt Rogal') #nazwa do zmiany
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+            self.clock.tick(60) #to sa FPS, do ustawienia pozniej z (settings.py)
+if __name__ == '__main__':
+    game = Game()
+    game.run()
